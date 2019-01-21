6e2ae038abcafc7fb0e2e08a25de5bb1466ed6fd
Alejandro <alevelara@gmail.com>
2019-01-21 22:51:10 +0100

    End of section 13

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
 
 constructor(private http: HttpClient) { }
 
-changeMemberPhoto(photoUrl: string){
+changeMemberPhoto(photoUrl: string) {
   this.photoUrl.next(photoUrl);
 }
 
